7757b409979a295edd2af0a65ddb1a29a7386609
Takasakiii <lucasmc2709@live.com>
2021-11-05 11:09:11 -0300

    removendo arquivos de inicialização

---
+++ src/pages/_app.tsx
@@ -0,0 +1,9 @@
+import type { AppProps } from "next/app";
+
+import "tailwindcss/tailwind.css";
+
+function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
+
+export default App;
