0f436bcba2226483bf9d6be04b2eb062ff03fea0
Takasakiii <lucasmc2709@live.com>
2021-11-05 17:48:04 -0300

    just index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,13 @@
+import Link from "next/link";
+
 function IndexPage() {
-  return <></>;
+  return (
+    <>
+      <Link href="/login">
+        <a className="text-blue-600">Pagina de Login Base</a>
+      </Link>
+    </>
+  );
 }
 
 export default IndexPage;
