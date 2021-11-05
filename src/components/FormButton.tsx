2602da14b822b8de580b108c1ad201513f5595d6
Takasakiii <lucasmc2709@live.com>
2021-11-05 12:51:18 -0300

    Base da tela de cadastro

--- src/components/FormButton.tsx
+++ src/components/FormButton.tsx
@@ -1,12 +1,13 @@
 export interface FormButtonComponentProps {
   label: string;
+  className?: string;
 }
 
-function FormButtonComponent({ label }: FormButtonComponentProps) {
+function FormButtonComponent({ label, className }: FormButtonComponentProps) {
   return (
     <button
       type="submit"
-      className="p-2 border-solid border-2 border-gray-200 rounded-md hover:bg-gray-200 duration-75"
+      className={`p-2 border-solid border-2 border-gray-200 rounded-md hover:bg-gray-200 duration-75 ${className}`}
     >
       {label}
     </button>
--- src/pages/singup.tsx
+++ src/pages/singup.tsx
@@ -1,33 +1,27 @@
+import CenterScreenComponent from "../components/CenterScreen";
 import FormComponent from "../components/Form";
 import InputTextComponent from "../components/InputText";
 import FormButtonComponent from "../components/FormButton";
 import CenterComponent from "../components/Center";
-import CenterScreenComponent from "../components/CenterScreen";
 
-import Link from "next/link";
-
-function LoginPage() {
-  function onLogin() {
-    console.log("Login");
-  }
-
+function SingUpPage() {
   return (
     <CenterScreenComponent>
-      <FormComponent onSubmit={onLogin}>
+      <FormComponent>
         <CenterComponent>
-          <h1 className="text-2xl">Login</h1>
+          <h1 className="text-2xl">Cadastro</h1>
         </CenterComponent>
+        <InputTextComponent label="Nome Completo:" />
+        <InputTextComponent label="Nome Social:" />
+        <InputTextComponent label="Email:" />
         <InputTextComponent label="Usuario:" />
         <InputTextComponent label="Senha:" type="password" className="mb-2" />
         <CenterComponent>
-          <FormButtonComponent label="Entrar" className="mb-2" />
-          <Link href="/singup">
-            <a className="text-blue-600">Cadastre-se</a>
-          </Link>
+          <FormButtonComponent label="Cadastrar" />
         </CenterComponent>
       </FormComponent>
     </CenterScreenComponent>
   );
 }
 
-export default LoginPage;
+export default SingUpPage;
