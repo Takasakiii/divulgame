7b84ed7a7f14e5419ce6ab44f24fb1b85c2d6550
Takasakiii <lucasmc2709@live.com>
2021-11-15 15:53:08 -0300

    base card com imagem e devida formatacao

--- frontend/src/components/list-servicos/Card.tsx
+++ frontend/src/components/list-servicos/Card.tsx
@@ -1,5 +1,8 @@
 import { AnuncioForMany } from "../../api/api";
 
+import { Link } from "react-router-dom";
+
+import CrossSvg from "../../assets/svgs/iconmonstr-x-mark-4.svg";
 import "./Card.css";
 
 export interface CardComponentProps {
@@ -8,22 +11,27 @@ export interface CardComponentProps {
 
 function CardComponent(props: CardComponentProps) {
   return (
-    <div className="bg-white m-4 p-4 rounded-md flex card-component">
-      {props.anuncio.fotos.length > 0 ? (
-        <img
-          src="https://google.com/google.jpg"
-          alt="Foto inical do anuncio"
-          className="object-cover object-center mr-2"
-        />
-      ) : (
-        <></>
-      )}
-      <div className="w-full h-full">
-        <h1 className="text-center text-xl font-bold">
-          {props.anuncio.titulo}
-        </h1>
-        <p>{props.anuncio.descricao}</p>
-      </div>
+    <div className="bg-white m-4 rounded-md card-component">
+      <Link to="/" className="flex p-4">
+        {props.anuncio.fotos.length > 0 ? (
+          <img
+            src="https://google.com/google.jpg"
+            alt="Foto inical do anuncio"
+            className="object-cover object-center mr-2"
+          />
+        ) : (
+          <div className="no-image mr-2">
+            <img src={CrossSvg} alt="No Icon" />
+            <span>Sem Imagem</span>
+          </div>
+        )}
+        <div className="w-full h-full">
+          <h1 className="text-center text-xl font-bold">
+            {props.anuncio.titulo}
+          </h1>
+          <p>{props.anuncio.descricao}</p>
+        </div>
+      </Link>
     </div>
   );
 }
