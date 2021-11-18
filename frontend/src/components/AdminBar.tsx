c0bd1752f9a3ceb8b936a5394a29f3fe02fe1e49
Takasakiii <lucasmc2709@live.com>
2021-11-18 16:42:57 -0300

    admin bar com confirm delete

--- frontend/src/components/AdminBar.tsx
+++ frontend/src/components/AdminBar.tsx
@@ -1,18 +1,52 @@
+import React, { useState } from "react";
+import classNames from "classnames";
+
 import TrashSvg from "../assets/svgs/iconmonstr-trash-can-27.svg";
 import PencilSvg from "../assets/svgs/iconmonstr-pencil-7.svg";
 
 export interface AdminBarComponentProps {
   className?: string;
+  onDelete?: () => void;
+  onEdit?: () => void;
 }
 
 function AdminBarComponent(props: AdminBarComponentProps) {
+  const [confirmDelete, setConfirmDelete] = useState(false);
+
+  const confirmCss = "text-red-600";
+  const confirmIcon: React.CSSProperties = {
+    filter:
+      "invert(17%) sepia(94%) saturate(3484%) hue-rotate(351deg) brightness(94%) contrast(90%)",
+  };
+
+  function handleDelete() {
+    if (confirmDelete) {
+      props.onDelete && props.onDelete();
+    }
+
+    setConfirmDelete(!confirmDelete);
+  }
+
   return (
     <div className={`bg-white flex p-4 rounded-md ${props.className}`}>
-      <button className="mr-4 flex">
-        <img src={TrashSvg} alt="Excluir anuncio" className="mr-2" />
-        <span>Deletar Anuncio</span>
+      <button type="button" className="mr-4 flex" onClick={handleDelete}>
+        <img
+          src={TrashSvg}
+          style={confirmDelete ? confirmIcon : undefined}
+          alt="Excluir anuncio"
+          className="mr-2"
+        />
+        <span
+          className={classNames({
+            [confirmCss]: confirmDelete,
+          })}
+        >
+          {confirmDelete
+            ? "Clique novamente para confirmar"
+            : "Deletar Anuncio"}
+        </span>
       </button>
-      <button className="flex">
+      <button type="button" className="flex" onClick={props.onEdit}>
         <img src={PencilSvg} alt="Editar anuncio" className="mr-2" />
         <span>Editar Anuncio</span>
       </button>
